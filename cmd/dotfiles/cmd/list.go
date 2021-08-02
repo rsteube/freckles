13b17b7e9eae93abd7acc1f97e5b8e05173280a7
rsteube <rsteube@users.noreply.github.com>
2021-08-02 18:19:35 +0200

    added list

---
+++ cmd/dotfiles/cmd/list.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/rsteube/dotfiles-bin/pkg/dotfiles"
+	"github.com/spf13/cobra"
+)
+
+var listCmd = &cobra.Command{
+	Use:   "list",
+	Short: "list files",
+	Run: func(cmd *cobra.Command, args []string) {
+		dotfiles.Walk(func(dotfile dotfiles.Dotfile) error {
+			fmt.Println(dotfile.Path)
+			return nil
+		})
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
