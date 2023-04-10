6f5526e3a6caaec279d8d57e95c1fcc29864abbe
rsteube <rsteube@users.noreply.github.com>
2023-04-10 09:21:14 +0200

    updated carapace

--- cmd/freckles/cmd/git.go
+++ cmd/freckles/cmd/git.go
@@ -5,7 +5,7 @@ import (
 	"os/exec"
 
 	"github.com/rsteube/carapace"
-	"github.com/rsteube/carapace-bin/pkg/actions/bridge"
+	"github.com/rsteube/carapace-bridge/pkg/actions/bridge"
 	"github.com/rsteube/freckles/pkg/freckles"
 	"github.com/spf13/cobra"
 )
